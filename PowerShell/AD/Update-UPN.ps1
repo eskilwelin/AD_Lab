0ef2217ba7382532b22cd908c829fc4b87000e40
Eskil Welin <eskilwelin@gmail.com>
2026-08-19 16:49:15 +0200

    Synposis, example and giga best practice by Mr. Klåd

--- PowerShell/AD/Update-UPN.ps1
+++ PowerShell/AD/Update-UPN.ps1
@@ -1,3 +1,11 @@
+<#
+.SYNOPSIS
+    Bulk-updates UPN for users. Requires the new UPN to be added to the forest first.
+    Get-ADForest | Set-ADForest -UPNSuffixes @{add='nordvik.se'}
+.EXAMPLE
+    .\Update-UPN.ps1 -NewSuffix "nordvik.se" -OldSuffix "corp.nordvik.se" -WhatIf
+#>
+
 [CmdletBinding(SupportsShouldProcess=$true)]
 param(
     [Parameter(Mandatory=$true)][string]$OldSuffix,
@@ -14,11 +22,11 @@ function Get-Users{
         Where-Object UserPrincipalName -NotLike "*@$NewSuffix"
 }
 
-$Users = (Get-Users -OldSuffix $OldSuffix -NewSuffix $NewSuffix)
+$Users = Get-Users -OldSuffix $OldSuffix -NewSuffix $NewSuffix
 
 foreach ($User in $Users){
     if ($User.UserPrincipalName -notlike "*@$OldSuffix") { continue }
-    $NewUPN = $User.UserPrincipalName.Replace($OldSuffix,$NewSuffix)
+    $NewUPN = $User.UserPrincipalName -replace "@$([regex]::Escape($OldSuffix))$", "@$NewSuffix"
     if ($PSCmdlet.ShouldProcess($User.UserPrincipalName, "Updating UPN to $NewUPN")) {
         $User | Set-ADUser -UserPrincipalName $NewUPN
     }
